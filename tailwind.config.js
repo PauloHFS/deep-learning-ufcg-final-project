d994d00803b1dc06e854e87c08a352f9792781a9
Paulo Hernane <paulohernane10@gmail.com>
2025-09-06 20:36:38 -0300

    fix theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,19 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      background: '#121212',
-      surface: '#1e1e1e',
-      primary: '#6200ee',
-      'on-primary': '#ffffff',
-      'on-surface': '#e1e1e1',
-      'on-surface-secondary': '#a0a0a0',
-      white: '#ffffff',
-      black: '#000000',
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        background: '#121212',
+        surface: '#1e1e1e',
+        primary: '#6200ee',
+        'on-primary': '#ffffff',
+        'on-surface': '#e1e1e1',
+        'on-surface-secondary': '#a0a0a0',
+        white: '#ffffff',
+        black: '#000000',
+      },
     },
-    extend: {},
   },
   plugins: [],
 }
